a8e5a72b57609e0b2cfa6bab6daf26703c613c5d
wlix13 <mustafinrr.rm@gmail.com>
2026-03-12 23:38:32 +0300

    chore(release): bump version to 0.2.0

--- netloom/__init__.py
+++ netloom/__init__.py
@@ -1,6 +1,6 @@
 """NetLoom - Network topology orchestrator."""
 
-__version__ = "0.1.3"
+__version__ = "0.2.0"
 
 from .core.application import Application
 from .core.controller import BaseController
