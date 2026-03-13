eb53e49562b4fd364d0de49f301b16c0bcddc922
wlix13 <mustafinrr.rm@gmail.com>
2026-03-13 21:43:10 +0300

    refactor(infra): use VBoxManage wrapper and simplify controllers

--- netloom/core/__init__.py
+++ netloom/core/__init__.py
@@ -2,11 +2,49 @@
 
 from .application import Application
 from .controller import BaseController
+from .enums import (
+    FirewallAction,
+    FirewallImpl,
+    InterfaceKind,
+    NicModel,
+    NodeRole,
+    ParavirtProvider,
+    RoutingEngine,
+    TemplateSet,
+    TunnelType,
+    VBoxChipset,
+    VMControlAction,
+    VMStartType,
+    VMState,
+)
+from .errors import ConfigurationError, InfrastructureError, NetLoomError, TopologyError
 from .model import DisplayModel
+from .types import AppT
+from .vbox import VBoxManage, VBoxSettings
 
 
 __all__ = [
+    "AppT",
     "Application",
     "BaseController",
+    "ConfigurationError",
     "DisplayModel",
+    "FirewallAction",
+    "FirewallImpl",
+    "InfrastructureError",
+    "InterfaceKind",
+    "NetLoomError",
+    "NicModel",
+    "NodeRole",
+    "ParavirtProvider",
+    "RoutingEngine",
+    "TemplateSet",
+    "TopologyError",
+    "TunnelType",
+    "VBoxChipset",
+    "VBoxManage",
+    "VBoxSettings",
+    "VMControlAction",
+    "VMStartType",
+    "VMState",
 ]
